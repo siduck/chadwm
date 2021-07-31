f668851d30b4caf114a1d6f2bc8a347b2f1336dd
AnuragRai <kerneldeveloper7232@gmail.com>
2021-07-31 11:37:51 +0530

    Enable gcc arch native, linktime and level O3 optimizations

--- chadwm/config.mk
+++ chadwm/config.mk
@@ -27,7 +27,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -march=native -mtune=native -std=c99 -pedantic -Wall -Wno-deprecated-declarations -flto -O3 ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
