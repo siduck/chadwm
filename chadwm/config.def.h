a7106713e3ca538048d6753f811538c648edb84d
siduck76 <siduckk76@protonmail.com>
2021-08-02 18:33:47 +0530

    use feh instead of rofi (for tagpreview)

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -81,9 +81,10 @@ static const Rule rules[] = {
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    { "Gimp",     NULL,       NULL,       0,            1,           -1 },
-    { "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+       	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+      	{ "feh",      NULL,       NULL,       0,            0,           1,           -1 },
 };
 
 /* layout(s) */
