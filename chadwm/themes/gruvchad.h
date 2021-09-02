8992951c9e306f5f4cebb5e0a2abd845322ab20a
siduck76 <siduckk76@protonmail.com>
2021-09-02 19:23:50 +0530

    add gruvchad theme

---
+++ chadwm/themes/gruvchad.h
@@ -0,0 +1,11 @@
+static const char black[]       = "#222526";
+static const char gray2[]       = "#3b3e3f"; // unfocused window border
+static const char gray3[]       = "#5d6061";
+static const char gray4[]       = "#6d8dad";
+static const char blue[]        = "#6f8faf";  // focused window border
+static const char green[]       = "#89b482";
+static const char red[]         = "#ec6b64";
+static const char orange[]      = "#d6b676";
+static const char yellow[]      = "#d1b171";
+static const char pink[]        = "#cc7f94";
+static const char col_borderbar[]  = "#222526"; // inner border
