b222ee0a5eb3c668e0288add659c51c184f5ae02
siduck <siduck@tutanota.com>
2022-01-19 21:18:51 +0530

    add catppuccin theme

---
+++ chadwm/themes/catppuccin.h
@@ -0,0 +1,11 @@
+static const char black[]       = "#1E1D2D";
+static const char gray2[]       = "#282737"; // unfocused window border
+static const char gray3[]       = "#585767";
+static const char gray4[]       = "#282737";
+static const char blue[]        = "#96CDFB";  // focused window border
+static const char green[]       = "#ABE9B3";
+static const char red[]         = "#F28FAD";
+static const char orange[]      = "#F8BD96";
+static const char yellow[]      = "#FAE3B0";
+static const char pink[]        = "#d5aeea";
+static const char col_borderbar[]  = "#1E1D2D"; // inner border
