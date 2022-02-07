69d4fe763cb5f7ecdc57b73e8ba7ca46f31462e6
sioodmy <81568712+sioodmy@users.noreply.github.com>
2022-02-07 15:45:33 +0100

    Make moving and resizing windows more smooth
    This will fix lagging when moving and resizing windows on higher refresh rate monitors

--- chadwm/dwm.c
+++ chadwm/dwm.c
@@ -1172,7 +1172,7 @@ void dragcfact(const Arg *arg) {
       handler[ev.type](&ev);
       break;
     case MotionNotify:
-      if ((ev.xmotion.time - lasttime) <= (1000 / 60))
+      if ((ev.xmotion.time - lasttime) <= (1000 / 120))
         continue;
       lasttime = ev.xmotion.time;
       if (prev_x == -999999) {
