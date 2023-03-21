8682a874a77c87e6175fdcc2bcc23476e3de719b
sweettem <freebittersweet@proton.me>
2023-03-21 11:07:09 -0400

    can recognize both BAT0 and BAT1

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -1,45 +1,78 @@
-#!/bin/dash
+#!/bin/env bash
 
-# ^c$var^ = fg color
-# ^b$var^ = bg color
+# This script sets up a custom status bar for the dwm window manager.
+# The status bar includes information such as CPU load, battery level,
+# package updates, brightness, memory usage, wireless network status, and
+# the current time.
+
+# Color codes for the status bar. These can be used in the format ^c$color^ and ^b$color^
+# to set the foreground and background colors of different sections of the bar.
 
 interval=0
+# Directory where the theme files are located.
+themes_dir="$HOME/.config/chadwm/scripts/bar_themes/"
 
-# load colors
-. ~/.config/chadwm/scripts/bar_themes/onedark
+# Name of the current theme and available themes.
+current_theme="catppuccin"
+
+# Load the color variables from the theme file.
+if [ -f "${themes_dir}/${current_theme}" ]; then
+  . "${themes_dir}/${current_theme}"
+else
+  echo "Theme not found: ${current_theme}"
+  exit 1
+fi
 
 cpu() {
   cpu_val=$(grep -o "^[^ ]*" /proc/loadavg)
-
-  printf "^c$black^ ^b$green^ CPU"
-  printf "^c$white^ ^b$grey^ $cpu_val"
+  printf "%s" "^c$black^ ^b$green^ CPU"
+  printf "%s" "^c$white^ ^b$grey^ $cpu_val"
 }
 
 pkg_updates() {
-  #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
+   #updates=$({ timeout 20 doas xbps-install -un 2>/dev/null || true; } | wc -l) # void
   updates=$({ timeout 20 checkupdates 2>/dev/null || true; } | wc -l) # arch
   # updates=$({ timeout 20 aptitude search '~U' 2>/dev/null || true; } | wc -l)  # apt (ubuntu, debian etc)
-
-  if [ -z "$updates" ]; then
-    printf "  ^c$green^    Fully Updated"
+  
+  if [ "$updates" -eq 0 ]; then
+    printf "%s" "  ^c$green^    Fully Updated"
   else
-    printf "  ^c$green^    $updates"" updates"
+    printf "%s" "  ^c$green^    $updates"" updates"
   fi
 }
 
 battery() {
-  get_capacity="$(cat /sys/class/power_supply/BAT1/capacity)"
-  printf "^c$blue^   $get_capacity"
+  get_status=$(</sys/class/power_supply/BAT?/status)
+  get_capacity=$(</sys/class/power_supply/BAT?/capacity)
+  # Set battery icon based on capacity and status
+case "$get_status" in
+    "Discharging")
+        case "$get_capacity" in
+            9[0-9]|100) icon=" " ;;
+            7[5-9]|8[0-9]) icon=" " ;;
+            4[0-9]|5[0-9]|6[0-9]) icon=" " ;;
+            1[0-9]|2[0-9]|3[0-9]) icon=" " ;;
+            *) icon=" " ;;
+        esac
+        ;;
+    "Charging"|"Full")
+        icon=" "
+        ;;
+    *)
+        icon="?"
+        ;;
+esac
+printf "^c$blue^ %s $icon $get_capacity"
 }
 
 brightness() {
-  printf "^c$red^   "
-  printf "^c$red^%.0f\n" $(cat /sys/class/backlight/*/brightness)
+  printf "%s ^c$red^   "
+  printf "^c$red^%.0f\n %s" "$(cat /sys/class/backlight/*/brightness)" 
 }
 
 mem() {
-  printf "^c$blue^^b$black^  "
-  printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
+  printf "%s" "^c$blue^^b$black^  "
+  printf  "%s" "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
 }
 
 wlan() {
@@ -50,13 +83,13 @@ wlan() {
 }
 
 clock() {
-	printf "^c$black^ ^b$darkblue^ 󱑆 "
-	printf "^c$black^^b$blue^ $(date '+%H:%M')  "
+	printf "%s" "^c$black^ ^b$darkblue^ 󱑆 "
+  printf "%s" "^c$black^^b$blue^$(date '+%b %d %Y - (%H:%M)') "
 }
 
 while true; do
 
-  [ $interval = 0 ] || [ $(($interval % 3600)) = 0 ] && updates=$(pkg_updates)
+  [ $interval = 0 ] || [ $((interval % 3600)) = 0 ] && updates=$(pkg_updates)
   interval=$((interval + 1))
 
   sleep 1 && xsetroot -name "$updates $(battery) $(brightness) $(cpu) $(mem) $(wlan) $(clock)"
