347f3ef1b8506431fe503b731907a9fb3444f062
camel_case <101834410+utfeight@users.noreply.github.com>
2023-11-15 00:18:07 +0000

    fix: nixos specific no such file or directory (#172)

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,5 @@ feh --bg-fill ~/Pictures/wall/gruv.png &
 xset r rate 200 50 &
 picom &
 
-~/.config/chadwm/scripts/bar.sh &
+dash ~/.config/chadwm/scripts/bar.sh &
 while type chadwm >/dev/null; do chadwm && continue || break; done
