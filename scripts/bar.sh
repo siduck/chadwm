adcfea8c8ea1c8a4958970725c24eeb3e1a77b2b
Prateek Shukla <prateekshukla1108@gmail.com>
2024-12-23 20:59:53 +0530

    Update bar.sh
    Kept the default glyphs
    removed the volume applet
    changed update status to update every 1 hour

--- scripts/bar.sh
+++ scripts/bar.sh
@@ -28,17 +28,6 @@ brightness() {
   printf "^c$red^%.0f\n" "$(cat /sys/class/backlight/*/brightness)"
 }
 
-volume() {
-  vol=$(amixer get Master | grep -o '[0-9]*%' | head -n 1)
-  muted=$(amixer get Master | grep -o '\[off\]' | head -n 1)
-
-  if [[ "$muted" == "[off]" ]]; then
-    printf "^c$yellow^  Muted"  # Muted glyph
-  else
-    printf "^c$green^   $vol"  # Larger volume glyph
-  fi
-}
-
 mem() {
   printf "^c$blue^^b$black^  "
   printf "^c$blue^ $(free -h | awk '/^Mem/ { print $3 }' | sed s/i//g)"
@@ -46,13 +35,13 @@ mem() {
 
 wlan() {
   case "$(cat /sys/class/net/wl*/operstate 2>/dev/null)" in
-    up) printf "^c$black^ ^b$blue^   ^d^%s" " ^c$blue^Connected" ;;
-    down) printf "^c$black^ ^b$blue^ 睊 ^d^%s" " ^c$blue^Disconnected" ;;
+    up) printf "^c$black^ ^b$blue^ 󰤨  ^d^%s" " ^c$blue^Connected" ;;
+    down) printf "^c$black^ ^b$blue^ 󰤭 ^d^%s" " ^c$blue^Disconnected" ;;
   esac
 }
 
 clock() {
-  printf "^c$black^ ^b$darkblue^  "
+  printf "^c$black^ ^b$darkblue^ 󱑆 "
   printf "^c$black^^b$blue^ $(date '+%H:%M') "
 }
 
@@ -60,5 +49,5 @@ clock() {
 while true; do
   updates=$(pkg_updates) # Update every loop now. Less resource intensive than checking every second.
   xsetroot -name "$updates $(battery) $(brightness) $(volume) $(mem) $(wlan) $(clock)"
-  sleep 1
+  sleep 3600
 done
